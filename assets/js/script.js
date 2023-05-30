f586a7f79fa5d3692cca58b3bc8f4ab3c741b2f2
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-05-30 14:06:59 -0500

    pulling data from apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 console.log("test");
+
+fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=owAxRtDuPwIoxlebtT48GgfdkDkJBIlI")
+    .then(function(response) {
+      return response.json();  
+    })
+    .then(function(data) {
+      console.log(data);
+    
+      }
+    );
+
+ fetch("https://api.seatgeek.com/2/events?client_id=MjUxNTU1NTR8MTY4NTQ3MTQ0OC4wNTE2MTE3&geoip=98.213.245.205&range=12mi")
+    .then(function(response) {
+      return response.json();  
+    })
+    .then(function(data) {
+      console.log(data);
+    
+      }
+    );
+
+fetch("https://api.openbrewerydb.org/v1/breweries?by_dist=42.324,-88.9541&per_page=3")
+    .then(function(response) {
+      return response.json();  
+    })
+    .then(function(data) {
+      console.log(data);
+    
+      }
+    );
+
