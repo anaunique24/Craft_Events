350f252e37a161f711265a778b9a14928778353f
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-05-31 21:44:32 -0500

    working on the logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,50 +2,82 @@ console.log("test");
 var locationForm = document.querySelector('#location-form');
 var locationInput = document.querySelector('#location.input');
 var intrestInput = document.querySelectorAll('#intrests');
+var lat;
+var lon;
+var seetGeekAPI;
+var geoAPI;
+var now=dayjs().format("YYYY-MM-DD")
+console.log(now) 
 
-var now=dayjs().format("YYYY-MM-DDTHH:mm:ssZ[Z]") 
-console.log(now)
 
 
-var gatherAPI = function(event){
+function gatherAPI(event){
   event.preventDefault();
   // console.log("running");
   var location= locationInput.value.trim();
+  geoAPI= 'https://api.openweathermap.org/geo/1.0/direct?q=' + location +',840&limit=1&appid=e7ef61c6ce67516bc22001eacd3518fd'
+  fetch(geoAPI)
+  .then(function(response) {
+      
+      return response.json();
+  })
+  .then(function(data) {
+    for (var i = 0; i < data.length; i++) {
+    lat = data[i].lat;
+    lon = data[i].lon;
+    console.log(lat,lon)
+    seetGeekAPI = "https://api.seatgeek.com/2/recommendations?client_id=MjUxNTU1NTR8MTY4NTQ3MTQ0OC4wNTE2MTE3&lat="+lat+"&lon="+lon+"&datetime_utc.gt="+now+"&events.id=1162104"
 
-  
-
+    }
+   console.log(seetGeekAPI)
+    
+   
+  })
+    .catch(error => console.log(error));
+    
 }
 
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=owAxRtDuPwIoxlebtT48GgfdkDkJBIlI")
-    .then(function(response) {
-      return response.json();  
-    })
-    .then(function(data) {
-      console.log(data);
-    
-      }
-    );
 
- fetch("https://api.seatgeek.com/2/events?client_id=MjUxNTU1NTR8MTY4NTQ3MTQ0OC4wNTE2MTE3&geoip=98.213.245.205&range=12mi")
+function seetGeekRec(event){
+;  console.log()
+  var location= locationInput.value.trim();
+  var seekGeekAPI="https://api.seatgeek.com/2/recommendations?client_id=MjUxNTU1NTR8MTY4NTQ3MTQ0OC4wNTE2MTE3&city="+location+"&datetime_utc.gt="+now+"&events.id=1162104"
+  console.log(seekGeekAPI)
+ fetch("https://api.seatgeek.com/2/recommendations?client_id=MjUxNTU1NTR8MTY4NTQ3MTQ0OC4wNTE2MTE3&city="+location+"&datetime_utc.gt="+now+"&events.id=1162104")
     .then(function(response) {
       return response.json();  
     })
     .then(function(data) {
       console.log(data);
-      
+      for (var i = 0; i < data.length; i++){
+        title = recomdations.event.title
+        console.log(title)
+      }
     
       }
     );
+  }
 
-fetch("https://api.openbrewerydb.org/v1/breweries?by_dist=42.324,-88.9541&per_page=3")
-    .then(function(response) {
-      return response.json();  
-    })
-    .then(function(data) {
-      console.log(data);
-    
-      }
-    );
 
     dayjs().format()
-    locationForm.addEventListener('submit', gatherAPI);
+    locationForm.addEventListener('submit', gatherAPI)
+
+// fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=owAxRtDuPwIoxlebtT48GgfdkDkJBIlI")
+//     .then(function(response) {
+//       return response.json();  
+//     })
+//     .then(function(data) {
+//       console.log(data);
+    
+//       }
+//     );
+
+// fetch("https://api.openbrewerydb.org/v1/breweries?by_dist=42.324,-88.9541&per_page=3")
+//     .then(function(response) {
+//       return response.json();  
+//     })
+//     .then(function(data) {
+//       console.log(data);
+    
+//       }
+//     );
